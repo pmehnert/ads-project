87682dc254ce0e1353e775eaf37315615718793c
Pascal Mehnert <pascalmehnert@posteo.de>
2023-05-21 21:12:23 +0200

    feat: improve integer traits

--- src/int.rs
+++ src/int.rs
@@ -5,15 +5,15 @@ use std::fmt;
 /// Integers that may be used to store indices.
 ///
 /// Types implementing this trait are guaranteed to be unsigned integers with
-/// values ranges of [`ZERO`] ..= [`MAX`].
+/// value ranges of [`ZERO`](Self::ZERO) ..= [`MAX`](Self::MAX).
 ///
 /// This traits is _sealed_ and can therefore not be implemented on foreign types.
-///
-/// [`ZERO`]: Self::ZERO
-/// [`MAX`]: Self::MAX
 pub trait IndexInt:
     'static + Sized + Copy + TryFrom<usize> + fmt::Display + sealed::Sealed
 {
+    /// The size of `Self` in bits.
+    const BITS: u32;
+
     /// Representation of `0` for `Self`.
     const ZERO: Self;
 
@@ -27,21 +27,27 @@ pub trait IndexInt:
     fn to_usize(self) -> usize;
 }
 
+/// Integers with a corresponding smaller integer type.
+///
+/// This traits is _sealed_ and can therefore not be implemented on foreign types.
+pub trait AsHalfSize: sealed::Sealed {
+    /// An integer type with at least half as many bits as `Self`.
+    type HalfSize;
+}
+
 /// Primitive conversion of integers to [`IndexInt`]s.
 pub trait AsIndex: Sized + sealed::Sealed {
     /// Converts `self` to an instance of `Idx` using a primitve cast.
     fn to_index<Idx: IndexInt>(self) -> Idx;
 }
 
-impl AsIndex for usize {
-    fn to_index<Idx: IndexInt>(self) -> Idx { Idx::from_usize(self) }
-}
-
 #[doc(hidden)]
-macro_rules! impl_index_int {
-    ($($type:ty),* $(,)?) => {
+macro_rules! impl_integer_traits {
+    ($($type:ty => $half_size:ty),* $(,)?) => {
         $(
+            impl sealed::Sealed for $type {}
             impl IndexInt for $type {
+                const BITS: u32 = <$type>::BITS;
                 const ZERO: Self = 0;
                 const MAX: Self = <$type>::MAX;
 
@@ -52,18 +58,27 @@ macro_rules! impl_index_int {
 
                 fn to_usize(self) -> usize { self as usize }
             }
+            impl AsHalfSize for $type {
+                type HalfSize = $half_size;
+            }
         )*
     };
 }
 
-impl_index_int!(usize, u64, u32, u16, u8);
+impl_integer_traits!(u64 => u32, u32 => u16, u16 => u8, u8 => u8);
+
+#[cfg(target_pointer_width = "64")]
+impl_integer_traits!(usize => u32);
+#[cfg(target_pointer_width = "32")]
+impl_integer_traits!(usize => u16);
+#[cfg(target_pointer_width = "16")]
+impl_integer_traits!(usize => u8);
+
+impl AsIndex for usize {
+    fn to_index<Idx: IndexInt>(self) -> Idx { Idx::from_usize(self) }
+}
 
 #[doc(hidden)]
 mod sealed {
     pub trait Sealed {}
-    impl Sealed for usize {}
-    impl Sealed for u64 {}
-    impl Sealed for u32 {}
-    impl Sealed for u16 {}
-    impl Sealed for u8 {}
 }
