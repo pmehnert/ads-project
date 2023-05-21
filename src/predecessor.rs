f17a4e7d21456fb6bfa8df83ad22cdcf441d4d54
Pascal Mehnert <pascalmehnert@posteo.de>
2023-05-21 14:53:52 +0200

    refactor: clean up Elias-Fano implementation

--- src/predecessor.rs
+++ src/predecessor.rs
@@ -1,70 +1,82 @@
-//! TODO predecessor queries
+//! Data structures for accelerating predecessor queries.
+//!
+//! # Predecessor Problem
+//!
+//! Given a _universe_ `U = 0..u` and a sorted sequence `A` of `n` integers from
+//! `U`, for any `x ∈ U` the _predecessor_ of `x` in `A` is defined as
+//!
+//! > `pred(x, A) = max {y ∈ A | y <= x}`.
 
-use std::{iter::Peekable, slice};
+use std::{iter, iter::FusedIterator, slice};
 
 use crate::{
     bitvec::{flat_popcount::FlatPopcount, BitVec},
     packed::PackedArray,
 };
 
+/// An implementation of the quasi-succinct Elias-Fano coding for answering
+/// predecessor queries.
+///
+/// Each value is split into an upper and a lower half. The upper halves are
+/// stored in a bit vector using a kind of unary encoding, the lower halves are
+/// stored in a byte packed array.
 #[derive(Debug, Default, Clone)]
 pub struct EliasFano {
     upper_half: FlatPopcount<BitVec>,
     lower_half: PackedArray,
-    lower_bits: u32,
     maximum: Option<u64>,
 }
 
 impl EliasFano {
-    // todo change this to accept an iterator?
+    /// Constructs the Elias-Fano coding for the given sequence of integers.
+    ///
+    /// The elements of `values` must be sorted in ascending order. If they are
+    /// not, the program may panic or produce unexpected results, but will not
+    /// result in undefined behaviour.
+    ///
+    /// **Note:** This should really accept an iterator not a slice.
     pub fn new(values: &[u64]) -> Self {
+        // todo ignore equal MSBs of values to be able to partition input
         let maximum = match values.last() {
             Some(last) => *last,
             None => return Default::default(),
         };
 
-        // todo ignore equal MSBs of values
-        // let (minimum, maximum) = match values {
-        //     [first, .., last] => (first, last),
-        //     [single] => todo!(),
-        //     [] => return Default::default(),
-        // };
-
         let unused_bits = maximum.leading_zeros();
-        let upper_len = values.len().next_power_of_two().ilog2();
-        let upper_bits = unused_bits + upper_len;
-        let lower_bits = 64 - upper_bits;
+        let upper_bits = values.len().next_power_of_two().ilog2();
+        let lower_bits = 64u32.saturating_sub(upper_bits + unused_bits).max(1);
 
-        let upper_half: BitVec = EliasFanoUpperIter::new(values, lower_bits).collect();
-        let upper_half = FlatPopcount::new(upper_half);
+        let upper_iter = EliasFanoUpperIter::new(values, lower_bits);
+        let upper_half = FlatPopcount::new(upper_iter.collect());
         let lower_half = PackedArray::new(lower_bits, values.iter().copied());
 
-        Self { upper_half, lower_half, lower_bits, maximum: Some(maximum) }
+        Self { upper_half, lower_half, maximum: Some(maximum) }
     }
 
+    /// Estimates the data structure's allocation size in bits.
     pub fn size_bits(&self) -> usize {
-        self.upper_half.size_bits()
-            + self.upper_half.bitvec().size_bits()
+        self.upper_half.bitvec().size_bits()
+            + self.upper_half.size_bits()
             + self.lower_half.size_bits()
     }
 
+    /// Retuns the [predecessor](crate::predecessor#predecessor-problem) of
+    /// `value` in the integer array encoded by this instance.
     pub fn predecessor(&self, value: u64) -> Option<u64> {
-        let value_upper = value >> self.lower_bits;
+        let value_upper = value >> self.lower_half.int_bits();
+        let value_lower = value & self.lower_half.mask();
         if value_upper >= self.upper_half.count_zeros() {
             return self.maximum;
         }
 
-        // todo change bitvec and flat-popcount to use u64 instead of usize?
-
+        // Find index of values in `lower_half` with equal upper half as `value`
         let bitvec_index = self.upper_half.select0(value_upper);
         let lower_index = bitvec_index as u64 - value_upper;
         if lower_index == 0 {
             return None;
         }
 
-        let mask = u64::MAX >> (64 - self.lower_bits);
-        let value_lower = value & mask;
-
+        // Scan through consecutive `1`-bits in `upper_half` bit vector
         let mut bitvec_indexes = (1..=bitvec_index).rev();
         let pred = bitvec_indexes
             .by_ref()
@@ -72,25 +84,25 @@ impl EliasFano {
             .zip(self.lower_half.iter().take(lower_index as usize).rev())
             .find(|(_, pred_lower)| *pred_lower <= value_lower);
 
-        if let Some((_, pred_lower)) = pred {
-            Some((value & !mask) | pred_lower)
-        } else if let Some(bitvec_index) = bitvec_indexes.next() {
-            let rank = bitvec_index as u64 - value_upper;
-            let bitvec_index = self.upper_half.select1(rank);
-            let pred_upper = bitvec_index as u64 - rank;
-            let pred_lower = self.lower_half.index(rank as usize);
+        match pred {
+            Some((_, pred_lower)) => Some((value & !self.lower_half.mask()) | pred_lower),
+            None => {
+                // Find nearest value with a smaller upper half
+                let rank = bitvec_indexes.next()? as u64 - value_upper;
+                let bitvec_index = self.upper_half.select1(rank);
+                let pred_upper = bitvec_index as u64 - rank;
+                let pred_lower = self.lower_half.index(rank as usize);
 
-            Some((pred_upper << self.lower_bits) | pred_lower)
-        } else {
-            None
+                Some((pred_upper << self.lower_half.int_bits()) | pred_lower)
+            },
         }
     }
 }
 
-
+/// An iterator over the upper half bits for the Elias-Fano coding of a sequence of integers.
 #[derive(Debug, Clone)]
 struct EliasFanoUpperIter<'a> {
-    values: Peekable<slice::Iter<'a, u64>>,
+    values: iter::Peekable<slice::Iter<'a, u64>>,
     shift: u32,
     current: u64,
     remaining: usize,
@@ -130,6 +142,8 @@ impl<'a> Iterator for EliasFanoUpperIter<'a> {
 
 impl<'a> ExactSizeIterator for EliasFanoUpperIter<'a> {}
 
+impl<'a> FusedIterator for EliasFanoUpperIter<'a> {}
+
 #[cfg(test)]
 mod test {
     use std::{iter::repeat, iter::zip, mem::replace};
@@ -144,6 +158,16 @@ mod test {
         for i in 0..10 {
             assert_eq!(None, pd.predecessor(i));
         }
+    }
+
+    #[test]
+    fn test_single_element() {
+        let pd = EliasFano::new(&[0]);
+        (0..10).for_each(|i| assert_eq!(Some(0), pd.predecessor(i)));
+
+        let pd = EliasFano::new(&[10]);
+        (0..10).for_each(|i| assert_eq!(None, pd.predecessor(i)));
+        (10..20).for_each(|i| assert_eq!(Some(10), pd.predecessor(i)));
     }
 
     #[test]
